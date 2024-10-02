4089c5dea23781fc0f3339e65c607e90a0852119
adityanarayan26 <adityanarayanraikoti@gmail.com>
2024-10-02 18:50:42 +0530

    origin changed

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: '*',
+    origin: 'https://mern-chat-objrkdky9-adityanarayan26s-projects.vercel.app',
     credentials: true
 }));
 app.use('/api/auth', authRoutes)
