742cba20593519aa8c0758d727924868405ae86a
adityanarayan26 <adityanarayanraikoti@gmail.com>
2024-10-02 16:27:18 +0530

    origin changed to *

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true // Allow credentials (cookies)
 }));
 app.use('/api/auth', authRoutes)
