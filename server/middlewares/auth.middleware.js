925ce75e71bb7c14b346bc2f654836079d2e5a57
adityanarayan26 <niu_33252@niu.edu.in>
2025-01-26 18:09:57 +0530

    tried to solve the error

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/user.model.js';
-
+import { config } from 'dotenv';
+config()
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.token;
